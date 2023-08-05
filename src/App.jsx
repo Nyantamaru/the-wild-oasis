f1e6476629d039db05ab1dab10e12d842a0a7a31
Tzuyu <manekete@gmail.com>
2023-08-04 22:29:03 -0600

    Building more reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.main`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `;
 
@@ -16,13 +17,32 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert('Check in')}>Check In</Button>
-        <Button onClick={() => alert('Check in')}>Check Out</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
 
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guess" />
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('Check in')}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+
+            <form>
+              <Input type="number" placeholder="Number of guess" />
+              <Input type="number" placeholder="Number of guess" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
